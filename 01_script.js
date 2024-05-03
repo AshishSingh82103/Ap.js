86cacd8876cb6f694d93fc16b90eda23a9d983c8
Ashish Singh <singhashish82103@gmail.com>
2024-05-03 15:45:03 +0530

    Done

--- 01_script.js
+++ 01_script.js
@@ -37,8 +37,29 @@ const student = {
 };
 student["name"] = 'rahul shrma';
 console.log(student["name"]); // rahul shrma
-student["age"] = student["age"] + 1;
+
 // console.log(student["age"]);
-console.log(student["age"]);
+
+student["age"] = student["age"] + 1;
+console.log(student["age"]); // 21
 
 console.log(student.cgpa);
+
+// practice One
+const product = {
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price:270,
+};
+console.log(product);
+
+// practice two
+const profile = {
+    userName: "singhashis@82103",
+    isFollow: "false",
+    followers: 321,
+    following: 123,
+};
+console.log(typeof profile); //object
+console.log(typeof profile.userName); //  string
