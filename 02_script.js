2529af27a76c0ca6811e93454a8a4b915677a597
Ashish Singh <singhashish82103@gmail.com>
2024-05-06 14:06:48 +0530

    Operators and Conditional Statements

--- 02_script.js
+++ 02_script.js
@@ -134,26 +134,56 @@
 // age >= 18 ? console.log("Adult") : console.log("not adult");
 
 // -----------Swtich statement------------------
-const foo = 0;
-switch (foo) {
-  case -1:
-    console.log("negative 1");
-    break;
-  case 0: // Value of foo matches this criteria; execution starts from here
-    console.log(0);
-  // Forgotten break! Execution falls through
-  case 1: // no break statement in 'case 0:' so this case will run as well
-    console.log(1);
-    break; // Break encountered; will not continue into 'case 2:'
-  case 2:
-    console.log(2);
-    break;
-  default:
-    console.log("default");
-    break;
-}
+// const foo = 0;
+// switch (foo) {
+//   case -1:
+//     console.log("negative 1");
+//     break;
+//   case 0: // Value of foo matches this criteria; execution starts from here
+//     console.log(0);
+//   // Forgotten break! Execution falls through
+//   case 1: // no break statement in 'case 0:' so this case will run as well
+//     console.log(1);
+//     break; // Break encountered; will not continue into 'case 2:'
+//   case 2:
+//     console.log(2);
+//     break;
+//   default:
+//     console.log("default");
+//     break;
+// }
 // Logs 0 and 1
 
+// Q : 1=> get user to input a number using prompt("Enter a number:").Check if the number is a multiple of 5 or not?
+// let num = prompt("Enter a number");
+// if(num % 5 === 0) {
+//     console.log(num, "is multiple of 5");
+// } else {
+//     console.log(num, "is not multiple of 5");
+// }
+
+// Q : 2=>write a code which can give a grades to student according to thier score
+// 80-100 A
+// 70-89, B
+// 60-69, C 
+// 50-59, D 
+// 0-49, F
+
+let score = 75;
+let grade;
+
+if( score >=80 && score <=100) {
+    grade = "A"
+} else if (score >=70 && score <= 89) {
+    grade = "B"
+} else if (score >=60 && score <= 69){
+    grade = "C"
+} else if (score >= 50 && score <= 59) {
+    grade = "D"
+} else if(score >= 0 && score <=49) {
+    grade = "F"
+}
+console.log("According to your scores, your grade was : ", grade)
 
 
 
