443c9696db73173c84e9d84262265d019bc72ca3
Ashish Singh <singhashish82103@gmail.com>
2024-05-08 11:40:28 +0530

    Arrays and methods

--- 05_script.js
+++ 05_script.js
@@ -74,7 +74,163 @@ function mul(a, b) {
 const arrowmulti = (a, b) => {
     console.log(a * b);
 }
-arrowmulti(3, 4);
+arrowmulti(3, 4); // 12
+
+const print = () => {
+    console.log("Ashish");
+}
+// print(); Ashish
+
+// Qs : Create a function using the "function" keyword that takes a String as an argument & returns the number of vowels in the string
+
+function countVowels (str) {
+    // ApnaCollege, count = 0
+    let count = 0;
+
+    for(const char of str) {
+        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+
+        }
+        
+    }
+
+    console.log(count); // 5
+}
+countVowels("apnacollage"); // 5
+
+// Qs: create a same problem with arrow function
+const countVow = (str) => {
+    let count = 0;
+
+    for(const char of str) {
+        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+
+        }
+        
+    }
+
+    console.log(count); // 5
+
+}
+countVow("apnacollege");
+
+
+// for each Loop in arrays
+
+// arr.forEach(callBackFunction)
+
+// callBackFunction : Headers, it is a function to execute for each element in the array
+// * A callback is a function passed as an argument to another function*
+
+// arr.forEach((val)=>{
+//     console,log(val);
+// })
+
+let arr = [1, 2, 3, 4, 5];
+// arr.forEach(function printVal(val) {
+//     console.log(val);
+// });
+
+arr.forEach((val, index, arr)=>{
+    console.log(val, index, arr);
+
+});
+
+// what is high order function or method =>Ans:  forEach
+
+// Qs : for a given arrays of numbers, print the square of each value using the for each loop.
+
+// let arrNum = [2, 3, 4, 5, 6];
+// arrNum.forEach((num) =>{
+//     console.log(num*num);
+// });
+
+// or
+let arrNum = [2, 3, 4, 5, 6];
+let calculateSquare = (num) =>{
+    console.log(num*num);
+};
+arrNum.forEach(calculateSquare);
+
+// Map method is very similar to for each but it create a new array
+// arr.map()
+let num = [2, 3, 4, 5];
+// num.map((val) =>{
+//     console.log(val);
+
+// })
+let newValue = num.map((val) => {
+    return val * val;
+})
+console.log(newValue);
+
+// filter method =>  create a new arr and filter it.
+
+let filterArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let evenArr = filterArr.filter((val)=>{
+    
+    return val % 2 === 0;
+
+})
+console.log(evenArr);
+
+//  reduce method => perform some opertions and reduce to it single value accumulator starting value eg 1 curr 2
+
+let reduceArr = [1, 2, 3, 4];
+ let result = reduceArr.reduce((accumulator, currentValue)=>{
+    return accumulator + currentValue;
+})
+console.log(result);
+
+// find the largest element in an Array
+let arr1 = [23, 45, 67, 74, 24];
+let result1 = arr1.reduce((accumulator, currentValue)=>{{
+    return accumulator > currentValue ? accumulator : currentValue;
+}});
+console.log(result1); // 74
+
+// Qs : We are given an array of marks of student. filter out of the marks of the student that scored 90+.
+let marks = [87, 97, 34, 49, 99, 96, 86];
+ let finalAns = marks.filter((val) =>{
+    return val > 90;
+
+})
+console.log(finalAns);
+
+// Qs : a. Take a number n as input from User. create an array of number from 1 to n.
+// b. use the reduce method to calculate the sum of the all number in array.
+// c. use the reduce method to calculate the prodcut of the all numbers in the array.
+
+// a.
+ let n = prompt('Enter a number: ');
+ let inputArr = [];
+ for(let i = 1; i <= n; i++) {
+    inputArr[i - 1] = i; //1(0), 2(1), 3(2)
+ }
+ console.log(inputArr);
+
+//  b.
+  let valueSum = inputArr.reduce((accumulator, currentValue) =>{
+    return accumulator + currentValue;
+
+ });
+ console.log(valueSum);
+
+ // c.
+  let vauleMulti = inputArr.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue;
+ })
+
+ console.log(vauleMulti);
+
+
+
+
+
+
 
 
 
