867497335d45e09c85614d5708dbbb58fee10b1f
Ashish Singh <singhashish82103@gmail.com>
2024-05-09 13:02:48 +0530

    Event

---
+++ 09_script.js
@@ -0,0 +1,26 @@
+// -----------Events-------------
+
+// let btn1 = document.querySelector("#btn1");
+// btn1.onclick = () => {
+//     console.log("btn1 was clicked");
+//     let a = 25;
+//     a++;
+//     console.log(a);
+// }
+
+// let div = document.querySelector("div");
+// div.onmouseover = () =>{
+//     console.log("this is box");
+//     let a = 25;
+//     a++;
+//     console.log(a);
+// }
+
+// -----------Event object------------
+
+// let btn1 = document.querySelector("#btn1");
+// btn1.onclick = (e) => {
+//     console.log(e);
+  
+// }
+
